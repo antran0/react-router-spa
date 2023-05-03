552e61186493faf700538c7e371172678aa2dc1b
Steven Tran <steven.tran550@gmail.com>
2023-05-03 14:10:20 -0700

    switched back to object based route definitions

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,8 +1,8 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  Route,
+  // Route,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
@@ -21,20 +21,20 @@ import ProductsPage from "./pages/Products";
 // Input: Array of route definition objects where each object
 //        represents one route
 // 1) define routes
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+const routeDefinitions = [
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+];
 
 /*
   An alternative way of defining routes.
 */
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter(routeDefinitions);
 
