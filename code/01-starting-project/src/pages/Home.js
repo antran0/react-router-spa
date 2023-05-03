aaa8d346b1582bdba1eebc0061505adc784b1070
Steven Tran <steven.tran550@gmail.com>
2023-05-03 14:36:55 -0700

    using the Link component to navigate to another page while avoiding having to send a new HTTP request

--- code/01-starting-project/src/pages/Home.js
+++ code/01-starting-project/src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = (props) => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        {/*
+          The Link component is the react-router-dom equivalent of the anchor
+          <a> tag in HTML. It uses an anchor tag under the hood and will update
+          the address bar with the new path as usual. However, the Link component
+          listens to clicks and prevents sending a new request to the server, thus
+          avoiding having to reload the page.
+          
+          Equivalent to:
+            <a href="/products">the list of products</a>
+         */}
+        Go to <Link to="/products">the list of products</Link>.
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
