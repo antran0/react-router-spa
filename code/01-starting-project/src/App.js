0b66e01bb0ca5d629dfacabee9eaa2db3892f2b5
Steven Tran <steven.tran550@gmail.com>
2023-05-03 02:47:52 -0700

    simple single page application with one page

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+/*
+  Adding routing to our application requires:
+    1) define the routes we want to support and which components should be
+    loaded for each path (URL -> component mapping)
+
+    2) activate the router and load the route definitions defined
+
+    3) make sure we have all these components we want to load and provide
+    some means of navigating between those pages
+*/
+
+// Input: Array of route definition objects where each object
+//        represents one route
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
