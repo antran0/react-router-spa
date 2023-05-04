b5c00009f4b667caf3e89e46b8ddf2f4d5889701
Steven Tran <steven.tran550@gmail.com>
2023-05-04 14:40:58 -0700

    added output to display the selected event id on the EventDetailPage

--- code/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ code/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,5 +1,24 @@
+import { useNavigate, useParams } from "react-router-dom";
+
 const EventDetailPage = () => {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    navigate("..");
+  };
+
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>{params.eventId}</p>
+      <p>
+        <button type="button" onClick={clickHandler}>
+          Back
+        </button>
+      </p>
+    </>
+  );
 };
 
 export default EventDetailPage;
