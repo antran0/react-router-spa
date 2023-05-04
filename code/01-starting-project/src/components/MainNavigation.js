48bbb8b086f4ca54ea84c6927c1ff919b888bb3a
Steven Tran <steven.tran550@gmail.com>
2023-05-03 17:34:48 -0700

    replaced Link with NavLink to display dynamic styles to the currently active route

--- code/01-starting-project/src/components/MainNavigation.js
+++ code/01-starting-project/src/components/MainNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
@@ -8,10 +8,33 @@ const MainNavigation = () => {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              // In the NavLink component, the className prop does not accept
+              // a string as usual, but a function that returns a string.
+              // An object {isActive, isPending} is automatically passed as
+              // an argument and we can use that to conditionally add style.
+              // ** The function form also works in the 'style' prop **
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              // Indicates this link should only be considered active if the
+              // currently active route ends with the path in the 'to' prop.
+              // Can take the value true or false or no value for true.
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
