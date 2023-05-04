03022b817f9b938e194f49058a989d1d99dab23f
Steven Tran <steven.tran550@gmail.com>
2023-05-04 14:27:05 -0700

    added a list of dummy events to the EventsPage and included a Link to their respective EventDetailPage

--- code/12-adv-starting-project/frontend/src/pages/Events.js
+++ code/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  { eventId: "e1", title: "Teddy Bear Exchange" },
+  { eventId: "e2", title: "Chili Cook Off" },
+  { eventId: "e3", title: "LOTR Helms Deep Re-enactment" },
+];
+
 const EventsPage = () => {
-  return <h1>Events Page</h1>;
+  const items = EVENTS;
+
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.eventId}>
+            <Link to={item.eventId}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
